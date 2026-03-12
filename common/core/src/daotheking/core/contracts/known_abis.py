c74f3cbdea7d8ebd2526d37fbc7abcb7754e1669
Luis Masuelli <luisfmasuelli@gmail.com>
2026-03-12 17:31:05 -0500

    Finishing the known ABIs.

--- common/core/src/daotheking/core/contracts/known_abis.py
+++ common/core/src/daotheking/core/contracts/known_abis.py
@@ -1171,7 +1171,53 @@ _USER_OP = {
     "internalType": "PackedUserOperation",
     "name": "userOp",
     "type": "tuple",
-    "components": []
+    "components": [
+        {
+            "internalType": "address",
+            "name": "sender",
+            "type": "address"
+        },
+        {
+            "internalType": "uint256",
+            "name": "nonce",
+            "type": "uint256"
+        },
+        {
+            "internalType": "bytes",
+            "name": "initCode",
+            "type": "bytes"
+        },
+        {
+            "internalType": "bytes",
+            "name": "callData",
+            "type": "bytes"
+        },
+        {
+            "internalType": "bytes32",
+            "name": "accountGasLimits",
+            "type": "bytes32"
+        },
+        {
+            "internalType": "uint256",
+            "name": "preVerificationGas",
+            "type": "uint256"
+        },
+        {
+            "internalType": "bytes32",
+            "name": "gasFees",
+            "type": "bytes32"
+        },
+        {
+            "internalType": "bytes",
+            "name": "paymasterAndData",
+            "type": "bytes"
+        },
+        {
+            "internalType": "bytes",
+            "name": "signature",
+            "type": "bytes"
+        }
+    ]
 }
 
 # Base ERC-4337 interface
